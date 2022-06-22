12d4b5e10547a50d7ef5e2da784792dafacd4142
KimWang906 <hyunbin06git@gmail.com>
2022-06-22 22:20:05 +0900

    make printf and msg

--- msg.asm
+++ msg.asm
@@ -1,7 +1,7 @@
 ;읽기 쓰기가 가능한 일반적인 데이터를 위한 영역이다.
 section .data 
     msg db "Hello World"
-    msg1 db "Assembly"
+    
     ;data영역에 Hello World라는 문자열을 만들어놓고, 
     ;이 위치를 가리키는 포인터 msg를 변수로 두었다.
 
@@ -10,14 +10,12 @@ section .data
 section .text
     global _start
     ;_start 함수는 main 함수보다 먼저 실행되는 함수를 의미한다.
+    golbal _start1
 
 _start:
     mov rax, 1 ;sys_write mode(쓰기)
     mov rdi, 1
     mov rsi, msg
-    mov rdx, 12
-    syscall
-    mov rsi, msg1
     mov rdx, 12
     syscall
     ;syscall이란, 프로세스가 하드웨어에 직접 접근해서 필요한 기능을 사용할 수 있게 해주는 기능이다.
